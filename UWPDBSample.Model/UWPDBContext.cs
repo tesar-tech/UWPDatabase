75d503dbaeae1c81e1a5bc0b4b90fea82303080d
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2021-03-21 17:41:06 +0100

    Add project files.

---
+++ UWPDBSample.Model/UWPDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace UWPDBSample.Model
+{
+    public class UWPDBContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=UWPDB.db");
+        }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
